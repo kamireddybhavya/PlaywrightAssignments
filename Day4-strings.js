974e2b7199e1838d69f549051bb25aa3428c2199
kamireddybhavya <111482190+kamireddybhavya@users.noreply.github.com>
2026-06-13 16:07:20 +0530

    Add files via upload

---
+++ Day4-strings.js
@@ -0,0 +1,23 @@
+let s ="Hello World";
+let str = s.split(" ");
+let word = str[str.length-1]
+console.log(word.length)
+
+
+
+let s1 = "   fly me   to   the moon  "
+let str1 = s1.trim().split(" ");
+let word1 = str1[str1.length-1]
+console.log(word1.length)
+
+function  isAnagram(w1, w2) {
+    let string1 = w1.trim().toLowerCase()
+    let string2 = w2.trim().toLowerCase()
+    string1 =  string1.split("").sort().join();
+    string2 =  string2.split("").sort().join();
+    return string1==string2
+        
+}
+console.log(isAnagram('listen', 'silent'));
+console.log(isAnagram('hello', 'world'));
+//'listen', 'silent'
