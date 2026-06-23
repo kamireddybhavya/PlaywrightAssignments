db3119e80a6fc7fda8fc3b239d92846a5b4f04f9
kamireddybhavya <111482190+kamireddybhavya@users.noreply.github.com>
2026-06-23 12:52:38 +0530

    Update Day1Classroom.spec.ts

--- Week3/Day1Classroom.spec.ts
+++ Week3/Day1Classroom.spec.ts
@@ -1,1 +1,13 @@
 
+import{test} from '@playwright/test';
+test('ParaBank Application', async({page})=>{
+    await page.goto("https://parabank.parasoft.com/parabank/index.htm")
+    await page.getByRole("link",{name:"Forgot login info?"}).click()
+    await page.getByAltText(`ParaBank`).click()
+    await page.getByText("Locations").nth(1).click()
+    
+    await page.getByRole("button").nth(1).click()
+    await page.locator(`//input[@name="search"]`).fill('chennai')
+})
+
+
