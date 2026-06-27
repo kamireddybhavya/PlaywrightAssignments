84810c0530f8d53073ab1654f2e6bf7c5aa6ed38
kamireddybhavya <111482190+kamireddybhavya@users.noreply.github.com>
2026-06-27 20:08:58 +0530

    Create Day4Assignment1.ts

---
+++ Week3/Day4Assignment1.ts
@@ -0,0 +1,16 @@
+//Factorial
+function factorial(n: number): number {
+
+    if (n < 0) {
+        throw new Error("Factorial is not defined for negative numbers.")
+    }
+    let result = 1
+    for (let i = 2; i <= n; i++) {
+        result = result*i
+    }
+    return result
+}
+console.log(factorial(5));
+console.log(factorial(3));
+console.log(factorial(0));
+console.log(factorial(-1));
