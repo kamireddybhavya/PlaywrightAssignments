0b54e7fd474df2cac09cebc5bab4aa7b7ce479d2
kamireddybhavya <111482190+kamireddybhavya@users.noreply.github.com>
2026-06-30 12:37:21 +0530

    Create day1Assignment.spec.ts

---
+++ week4/day1Assignment.spec.ts
@@ -0,0 +1,21 @@
+import {expect, test} from '@playwright/test';
+test('frames and alert',async({page})=>{
+    await page.goto(`https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm`)
+    page.on('dialog',async(alert)=>{
+        const msg = alert.message()
+         console.log(msg)
+        console.log(alert.type())
+        await alert.accept()
+
+    })
+    const frame = await page.frameLocator(`#iframeResult`)
+    const Beforetext = await frame.locator(`//button[text()='Try it']`).innerText()
+    console.log(Beforetext)
+    await frame.locator(`//button[text()='Try it']`).click()
+    await expect(frame.locator(`#demo`)).toHaveText(`You pressed OK!`)
+    const aftertext = await frame.locator(`#demo`).innerText()
+    console.log(aftertext)
+})
+
+
+
